46ec979bec81de6f126c8b00e8608579809471a5
Callum Smith <58210772+callumsmith2@users.noreply.github.com>
2025-06-12 12:41:51 +0800

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,8 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define CONFIG_ZMK_DISPLAY=y
-#define CONFIG_ZMK_DISPLAY_STATUS_SCREEN_CUSTOM=y
+
 
 #define MIRYOKU_LAYER_BASE \
 &kp GRAVE,          &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,                   &kp BSLH,   \
